78b4db4700ac53ca36d0326e42dc2f0c0881a6ba
Nikolai Kabiolskij <n.kabiolskij@gmail.com>
2022-08-04 14:46:12 +0200

    adjuts props

--- types/forms.d.ts
+++ types/forms.d.ts
@@ -57,7 +57,7 @@ export interface Service {
   unit: string;
   name: string;
   tenant: string;
-  customervisible: string;
+  customervisible: boolean;
   description: string;
   sortorder: number;
   colli: number;
